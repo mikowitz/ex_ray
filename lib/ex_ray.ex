383b789c98d6814b37ab564b91df7a395941ab6a
Michael Berkowitz <michael.berkowitz@gmail.com>
2021-04-08 22:11:15 -0400

    mix new

---
+++ lib/ex_ray.ex
@@ -0,0 +1,18 @@
+defmodule ExRay do
+  @moduledoc """
+  Documentation for `ExRay`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExRay.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
