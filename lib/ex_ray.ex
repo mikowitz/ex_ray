909e92c07ad985c99238202443a690bf1741e6f3
Michael berkowitz <michael.berkowitz@gmail.com>
2024-08-29 23:12:10 -0400

    2. Output an Image

--- lib/ex_ray.ex
+++ lib/ex_ray.ex
@@ -1,18 +1,36 @@
 defmodule ExRay do
-  @moduledoc """
-  Documentation for `ExRay`.
-  """
+  def generate do
+    image_width = 256
+    image_height = 256
 
-  @doc """
-  Hello world.
+    format = [
+      bar_color: [IO.ANSI.green_background()],
+      blank_color: [IO.ANSI.red_background()],
+      bar: " ",
+      blank: " ",
+      left: "",
+      right: ""
+    ]
 
-  ## Examples
+    File.open("test.ppm", [:write], fn file ->
+      IO.puts(file, "P3\n#{image_width} #{image_height}\n255")
 
-      iex> ExRay.hello()
-      :world
+      for y <- 0..(image_height - 1) do
+        for x <- 0..(image_width - 1) do
+          r = x / (image_width - 1)
+          g = y / (image_height - 1)
+          b = 0
 
-  """
-  def hello do
-    :world
+          ir = round(255.999 * r)
+          ig = round(255.999 * g)
+          ib = round(255.999 * b)
+
+          IO.puts(file, "#{ir} #{ig} #{ib}")
+          ProgressBar.render(y * image_width + x, image_width * image_height, format)
+        end
+      end
+    end)
+
+    IO.puts("")
   end
 end
