609aa0596addd55364e854217bc633a6dacb36b7
Michael Berkowitz <michael.berkowitz@gmail.com>
2024-09-03 14:55:34 -0400

    12. Positionable Camera (#11)

--- lib/ex_ray.ex
+++ lib/ex_ray.ex
@@ -5,14 +5,24 @@ defmodule ExRay do
     camera =
       Camera.new()
       |> then(fn c ->
-        %Camera{c | aspect_ratio: 16 / 9, image_width: 400, samples_per_pixel: 100, max_depth: 50}
+        %Camera{
+          c
+          | aspect_ratio: 16 / 9,
+            image_width: 400,
+            samples_per_pixel: 100,
+            max_depth: 50,
+            vfov: 20,
+            lookfrom: point(-2, 2, 1),
+            lookat: point(0, 0, -1),
+            vup: vector(0, 1, 0)
+        }
       end)
 
     ground_mat = ExRay.Material.Lambertian.new(color(0.8, 0.8, 0.0))
     center_mat = ExRay.Material.Lambertian.new(color(0.1, 0.2, 0.5))
     left_mat = ExRay.Material.Dielectric.new(1.5)
     bubble_mat = ExRay.Material.Dielectric.new(1 / 1.50)
-    right_mat = ExRay.Material.Metal.new(color(0.8, 0.6, 0.2), 1)
+    right_mat = ExRay.Material.Metal.new(color(0.8, 0.6, 0.2), 0.3)
 
     world =
       World.new()
